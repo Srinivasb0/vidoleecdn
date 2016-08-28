9faa695acadf3652334887489323feff9db28c9f
RAM NARESHYADHV <RAM NARESHYADHV>
2016-08-28 17:25:38 +0530

    modified database

--- vidos/settings/production.py
+++ vidos/settings/production.py
@@ -14,7 +14,7 @@ DATABASES['default'] =  dj_database_url.config()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Allow all host headers
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 SHARE_URL = "http://vidolee.com/?ref="
