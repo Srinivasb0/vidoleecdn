c11c6b748e64d4cd2e7a533c6d2a2e2ee318b3f3
RAM NARESHYADHV <RAM NARESHYADHV>
2016-08-31 07:36:44 +0530

    wsgi changes

--- vidos/wsgi.py
+++ vidos/wsgi.py
@@ -12,9 +12,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vidos.settings")
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
 
-try:
-    from dj_static import Cling
-    application = Cling(get_wsgi_application())
-except:
-    pass
+from dj_static import Cling, MediaCling
+application = Cling(get_wsgi_application())
+application = Cling(MediaCling(get_wsgi_application()))
 
